14777439dccb564aad980863ac36b232a8aecb15
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-07-27 17:38:26 -0400

    Formatting Text Update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,10 +23,10 @@ function NavBar() {
 
   return (
     <nav>
-      <h1> Job Applicaiton Tracker </h1>
+      <h1> Job Application Tracker </h1>
       <ul>
         <li>
-          <Link to='/'>Home</Link>
+          <Link to='/'> Home </Link>
         </li>
         <li>
           <Link to='/applications'>
@@ -34,7 +34,7 @@ function NavBar() {
           </Link>
         </li>
         <li>
-          <Link to='/applications/new'>New Application</Link>
+          <Link to='/applications/new'> New Application </Link>
         </li>
       </ul>
       {error && <p className='error'>{error}</p>}
